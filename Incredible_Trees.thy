93ea8832ad13352419555415a10ec3c5bc28f9e2
Joachim Breitner <mail@joachim-breitner.de>
2016-05-18 19:15:34 +0200

    Split out Inits theory

--- Incredible_Trees.thy
+++ Incredible_Trees.thy
@@ -3,6 +3,7 @@ imports
   "~~/src/HOL/Library/Sublist" 
   "~~/src/HOL/Library/Countable"
   Entailment
+  Inits
   Abstract_Rules_To_Incredible
 begin
 
@@ -475,21 +476,6 @@ using assms
   apply (case_tac x)
   apply (auto elim!: it_paths_ConsE dest: list_all2_nthD2)
   done
-
-fun inits where
-  "inits [] = [[]]"
-| "inits (i#is) = [] # map (op # i) (inits is)"
-
-lemma inits_Snoc[simp]:
-  "inits (is@[i]) = inits is @ [is@[i]]"
-by (induction "is") auto
-
-lemma inits_eq_Snoc:
-  "inits is = xs @ [x] \<longleftrightarrow> (is = [] \<and> xs = [] \<or> (\<exists> i is'. is = is'@[i] \<and> xs = inits is')) \<and> x = is"
-by (cases "is" rule: rev_cases) auto
-
-lemma in_set_inits[simp]: "is' \<in> set (inits is) \<longleftrightarrow> prefixeq is' is"
-  by (induction "is'" arbitrary: "is"; case_tac "is"; auto)
 
 definition isidx :: "nat list \<Rightarrow> nat" where "isidx xs = to_nat (Some xs)"
 definition v_away :: "nat" where "v_away = to_nat (None :: nat list option)"
---
+++ Inits.thy
@@ -0,0 +1,19 @@
+theory Inits
+imports Main "~~/src/HOL/Library/Sublist"
+begin
+
+fun inits where
+  "inits [] = [[]]"
+| "inits (i#is) = [] # map (op # i) (inits is)"
+
+lemma inits_Snoc[simp]:
+  "inits (is@[i]) = inits is @ [is@[i]]"
+by (induction "is") auto
+
+lemma inits_eq_Snoc:
+  "inits is = xs @ [x] \<longleftrightarrow> (is = [] \<and> xs = [] \<or> (\<exists> i is'. is = is'@[i] \<and> xs = inits is')) \<and> x = is"
+by (cases "is" rule: rev_cases) auto
+
+lemma in_set_inits[simp]: "is' \<in> set (inits is) \<longleftrightarrow> prefixeq is' is"
+  by (induction "is'" arbitrary: "is"; case_tac "is"; auto)
+end
