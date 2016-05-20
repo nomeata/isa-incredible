9993b17d1958b84e0ea08aac5d6b242edf64713d
Denis Lohner <denis.lohner@kit.edu>
2016-05-20 09:42:34 +0200

    refactor duplicate fact sset_cycle

--- Incredible_Propositional.thy
+++ Incredible_Propositional.thy
@@ -1,6 +1,7 @@
 theory Incredible_Propositional imports
   Abstract_Rules_To_Incredible
   Propositional_Formulas
+  Stream_Ext
 begin
 
 text \<open>Our concrete interpretation with propositional logic will cover conjunction and implication
@@ -13,21 +14,6 @@ datatype prop_rule = andI | andE | impI | impE
 
 definition prop_rules :: "prop_rule stream"
   where "prop_rules = cycle [andI, andE, impI, impE]"
-
-lemma sset_cycle [simp]:
-  assumes "xs \<noteq> []" 
-  shows "sset (cycle xs) = set xs"
-proof (intro set_eqI iffI)
-  fix x
-  assume "x \<in> sset (cycle xs)"
-  from this assms show "x \<in> set xs"
-    by (induction "cycle xs" arbitrary: xs rule: sset_induct) (case_tac xs; fastforce)+
-next
-  fix x
-  assume "x \<in> set xs"
-  with assms show "x \<in> sset (cycle xs)"
-    by (metis UnI1 cycle_decomp sset_shift)
-qed
 
 lemma iR_prop_rules [simp]: "sset prop_rules = {andI, andE, impI, impE}"
   unfolding prop_rules_def by simp
